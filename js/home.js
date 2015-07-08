dee5fe50ef56fae2f7d29af6e893bdc55d9bb604
WinnerJiang <winner_118@sohu.com>
2015-07-08 17:31:38 +0800

    update

---
+++ js/home.js
@@ -0,0 +1,16 @@
+/********************************
+ *        this file used for the home page
+ *        Author: WinnerJiang
+ *        Date: 2015-07-08
+ * 
+ ********************************/
+ 
+($document).ready(function{
+	
+	//Carousel Image
+	
+	
+	
+	
+	
+});
