4f7a7147f3430cca904c0a843ba89265bb09c711
WinnerJiang <winner_118@sohu.com>
2015-09-09 17:32:51 +0800

    0909

--- js/home.js
+++ js/home.js
@@ -5,11 +5,32 @@
  * 
  ********************************/
  
-($document).ready(function{
+($document).ready(function(){
+	
 	
 	//Carousel Image
+	var x=0;
+	var flag="go_to_left";
 	
+	var carouselImage = function(){
+		//for confine
+		if(x==-3240){
+			flag="go_to_right";
+		}
+		if(x==0){
+			flag="go_to_left";
+		}
+		if(flag=="go_to_right"){
+			$(".imgCarouselArea").animate({left:x},500);
+			x+=1080;
+		}
+		if(flag=="go_to_left"){
+			$(".imgCarouselArea").animate({left:x},500);
+			x-=1080;
+		}
+	};
 	
+	setInterval(carouselImage, 1000);
 	
 	
 	
